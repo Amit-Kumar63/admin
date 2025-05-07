03929b20a93607be65de20546f252b033e29c431
Amit-Kumar63 <amitkumarpro76@gmail.com>
2025-05-07 19:17:45 +0530

    improve home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='text-white'>
-      WTF Home page
+    <div className='text-white w-full'>
+      <h1>Home</h1>
+      <Link to={'/rarostock/dashboard'}>
+      <button className=' bg-white text-black py-2 px-4 rounded-md'>Dashboard</button>
+      </Link>
     </div>
   )
 }
